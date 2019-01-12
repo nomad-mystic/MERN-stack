f17ced67afbb1d41e1f306a5205f0423a249a900
kmurphy <kmurphy@casenetllc.com>
2019-01-11 16:21:55 -0800

    master: Initial Commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+// Init App
+const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+	.connect(db, {
+		useNewUrlParser: true
+	})
+	.then(() => {
+		console.log('MongoDB Connected')
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+const server = app.listen(port, () => console.log(`Server Running ${port}`));
+
+module.exports = server;
