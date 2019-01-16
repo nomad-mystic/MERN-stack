2cd32d8bfad29c2f692f441b25d7e68e4b0edd04
nomad-mystic <nomadmystics@gmail.com>
2019-01-15 17:01:37 -0800

    master: Worked out the login part of the auth for a user.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../../models/User');
+const keys = require('../../config/keys');
 
 // @route GET api/users/test
 // @desc Tests users route
@@ -16,7 +18,7 @@ router.get('/test', (req, res) => res.json({msg: "Users Works!!"}));
  * @route GET api/users/register
  * @desc Registers user
  * @access Public
- * @return {object} res | void
+ * @return {object} res
  */
 router.post('/register', (req, res) => {
 	User.findOne({
@@ -55,6 +57,61 @@ router.post('/register', (req, res) => {
 	.catch((err) => {
 		throw err;
 	});
+});
+
+/**
+ * @author Keith Murphy | nomadmystics@gmail.com
+ * @route GET api/users/login
+ * @desc Registers Login
+ * @access Public
+ * @return {string} token
+ */
+
+router.post('/login', (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+
+	// find by user email
+	User.findOne({email: email})
+		.then((user) => {
+			// check for user
+			if (!user) {
+				console.log('testing');
+				return res.status(404).json({email: 'User not found'});
+			}
+
+			bcrypt.compare(password, user.password)
+				.then((isMatch) => {
+					if (isMatch) {
+						// res.json({msg: 'success'});
+						// user Matched
+						const payload = {
+							id: user.id,
+							name: user.name,
+							avatar: user.avatar,
+						};
+
+						// Sign Token
+						jwt.sign(
+							payload,
+							keys.secret,
+							{ expiresIn: 3600, },
+							(err, token) => {
+								res.json({
+									success: true,
+									token: `Bearer ${token}`,
+								});
+							}
+						);
+					} else {
+						return res.status(400).json({password: 'Password incorrect'});
+					}
+				})
+				.catch((err) => {
+					throw err;
+				});
+		});
+
 });
 
 module.exports = router;
