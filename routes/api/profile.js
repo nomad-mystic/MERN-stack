e65528a95415c8bd36f96533597121e77fc51af6
nomad-mystic <nomadmystics@gmail.com>
2019-01-16 16:34:50 -0800

    section_4: Created base profile route for getting user profile.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// My modules
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route GET api/profile/test
 // @desc Tests profile route
 // @access Public
 router.get('/test', (req, res) => res.json({msg: "Profile Works!!"}));
+
+/**
+ * @author Keith Murphy | nomadmystics@gmail.com
+ * @route GET api/profile
+ * @desc Get the current users profile
+ * @access Private
+ * @return {object} res
+ */
+
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+	const errors = {};
+	Profile.findOne({user: req.user.id})
+		.then(profile => {
+			if (!profile) {
+				errors.noprofile = 'There is no Profile';
+				return res.status(404).json(errors);
+			}
+			res.json(profile);
+		})
+		.catch(err => {
+			res.status(404).json(err);
+		});
+});
+
+
+
 
 module.exports = router;
