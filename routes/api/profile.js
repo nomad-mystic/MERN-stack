e3a566d483c818a46d0d96270ebf3dfc7ab71e1f
nomad-mystic <nomadmystics@gmail.com>
2019-01-22 13:09:45 -0800

    master: Worked out a few different routes for the profiles.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -23,6 +23,7 @@ router.get('/test', (req, res) => res.json({msg: "Profile Works!!"}));
 router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
 	const errors = {};
 	Profile.findOne({user: req.user.id})
+		.populate('user', ['name', 'avatar'])
 		.then(profile => {
 			if (!profile) {
 				errors.noprofile = 'There is no Profile';
@@ -110,5 +111,92 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
 			res.status(404).json(err);
 		});
 });
+
+/**
+ * @author Keith Murphy | nomadmystics@gmail.com
+ * @route GET api/profile/all
+ * @desc Get all the profiles
+ * @access Public
+ * @return {object} res
+ */
+
+router.get('/all', (req, res) => {
+	const errors = {};
+
+	Profile.find()
+		.populate('user', ['name', 'avatar'])
+		.then(profiles => {
+			if (!profiles) {
+				errors.noprofile = 'There are no profiles.';
+				res.status(404).json(errors);
+			}
+
+			res.json(profiles);
+		})
+		.catch(err => res.status(404).json({profile: 'There are no profiles.'}));
+});
+
+/**
+ * @author Keith Murphy | nomadmystics@gmail.com
+ * @route GET api/profile/handle/:handle
+ * @desc Get the profile by handle
+ * @access Public
+ * @return {object} res
+ */
+
+router.get('/handle/:handle', (req, res) => {
+	const errors = {};
+
+	Profile.findOne({handle: req.params.handle})
+		.populate('user', ['name', 'avatar'])
+		.then(profile => {
+			if (!profile) {
+				errors.noprofile = 'There is not profile for this user';
+				res.status(404).json(errors);
+			}
+
+			res.json(profile);
+		})
+		.catch(err => res.status(404).json(err));
+});
+
+/**
+ * @author Keith Murphy | nomadmystics@gmail.com
+ * @route GET api/profile/user/:user_id
+ * @desc Get the profile by id
+ * @access Public
+ * @return {object} res
+ */
+
+router.get('/user/:user_id', (req, res) => {
+	const errors = {};
+
+	Profile.findOne({user: req.params.user_id})
+		.populate('user', ['name', 'avatar'])
+		.then(profile => {
+			if (!profile) {
+				errors.noprofile = 'There is not profile for this user';
+				res.status(404).json(errors);
+			}
+
+			res.json(profile);
+		})
+		.catch(err => res.status(404).json({profile: 'There is no profile for this user'}));
+});
+
+/**
+ * @author Keith Murphy | nomadmystics@gmail.com
+ * @route POST api/profile/experience
+ * @desc Add experience to profile
+ * @access Private
+ * @return {object} res
+ */
+
+router.post('/experience', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+
+});
+
+
 
 module.exports = router;
