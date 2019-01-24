c53cfebdbb20350c74805ff64f9fbe3c21c80d0a
nomad-mystic <nomadmystics@gmail.com>
2019-01-24 11:43:38 -0800

    section_4: Finsihed up the last few routes for the profile.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -231,32 +231,21 @@ router.post('/experience', passport.authenticate('jwt', {session: false}), (req,
  * @return {object} res
  */
 
-router.post('/experience/:exp_id', passport.authenticate('jwt', {session: false}), (req, res) => {
-	const {errors, isValid} = validateEducationInput(req.body);
-	// Check Validation
-	if (!isValid) {
-		return res.status(400).json(errors);
-	}
-
-	Profile.findOne({user: req.user.id })
+router.delete('/experience/:exp_id', passport.authenticate('jwt', {session: false}), (req, res) => {
+	Profile.findOne({user: req.user.id})
 		.then(profile => {
-			const newEdu = {
-				school: req.body.school,
-				degree: req.body.degree,
-				fieldOfStudy: req.body.fieldOfStudy,
-				from: req.body.from,
-				to: req.body.to,
-				current: req.body.current,
-				description: req.body.description,
-			};
+			// Get remove index
+			const removeIndex = profile.experience
+				.map(item => item.id)
+				.indexOf(req.params.exp_id);
 
-			// Add to exp array
-			profile.education.unshift(newEdu);
+			profile.experience.splice(removeIndex, 1);
 
 			profile.save().then(profile => res.json(profile)).catch(err => res.status(400).json({err}));
 		})
-		.catch(err => res.status(404))
+		.catch(err => res.status(404).json(err));
 });
+
 
 /**
  * @author Keith Murphy | nomadmystics@gmail.com
@@ -293,6 +282,47 @@ router.post('/education', passport.authenticate('jwt', {session: false}), (req, 
 		.catch(err => res.status(404))
 });
 
+/**
+ * @author Keith Murphy | nomadmystics@gmail.com
+ * @route DELETE api/profile/education/:edu_id
+ * @desc  Delete education to profile
+ * @access Private
+ * @return {object} res
+ */
 
+router.delete('/education/:edu_id', passport.authenticate('jwt', {session: false}), (req, res) => {
+	Profile.findOne({user: req.user.id})
+		.then(profile => {
+			// Get remove index
+			const removeIndex = profile.education
+				.map(item => item.id)
+				.indexOf(req.params.edu_id);
+
+			profile.education.splice(removeIndex, 1);
+
+			profile.save().then(profile => res.json(profile)).catch(err => res.status(400).json({err}));
+		})
+		.catch(err => res.status(404).json(err));
+});
+
+/**
+ * @author Keith Murphy | nomadmystics@gmail.com
+ * @route DELETE api/profile/education/:edu_id
+ * @desc  Delete profile and user
+ * @access Private
+ * @return {object} res
+ */
+
+router.delete('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+	Profile.findOneAndRemove({user: req.user.id})
+		.then(() => {
+			User.findOneAndRemove({_id: req.user.id})
+				.then(() => {
+					res.json({success: true});
+				})
+				.catch(err => res.status(404).json({err}));
+		})
+		.catch(err => res.status(404).json(err));
+});
 
 module.exports = router;
