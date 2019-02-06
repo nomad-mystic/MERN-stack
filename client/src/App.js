d121f6532ebe08cf5ea508d8188ef293d045e634
nomad-mystic <nomadmystics@gmail.com>
2019-02-06 13:14:59 -0800

    section_6: Base components have html in them still doub.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+
+/**
+ * @author Keith Murphy | nomadmystics@gmail.com
+ * @desc
+ */
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>My React App</h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Navbar/>
+				<Landing/>
+				<Footer/>
+			</div>
+		);
+	}
 }
 
 export default App;
