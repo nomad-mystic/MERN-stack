f1324cad3575926e7860e734828b13228b989b29
nomad-mystic <nomadmystics@gmail.com>
2019-02-06 14:28:19 -0800

    section_6: Built out some of the base routes from the landing page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,37 @@
-import React, { Component } from 'react';
+// libs
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+// styles
 import './App.css';
 
+// layout
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 
+// Auth
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
 /**
  * @author Keith Murphy | nomadmystics@gmail.com
- * @desc
+ * @desc This is the main class for our whole application
  */
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<Navbar/>
-				<Landing/>
-				<Footer/>
-			</div>
+			<Router>
+				<div className="App">
+					<Navbar/>
+					<Route exact path="/" component={Landing} />
+					<div className="container">
+						<Route exact path="/register" component={Register} />
+						<Route exact path="/login" component={Login} />
+					</div>
+					<Footer/>
+				</div>
+			</Router>
 		);
 	}
 }
